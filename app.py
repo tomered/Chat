f164c7f79e92d312f9725b99dea23569eeecbb91
tomer <tomeredel@gmail.com>
2025-03-12 11:19:57 +0200

    implement GET method

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 # Implemented by Tomer
-@app.route("/")
-def room():
-    return "hi"
-
+@app.route("/GET /<room>", methods= ['GET'])
+def room(room):
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
