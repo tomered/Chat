76c39fdae30fe1822813bb8eda300a7ef9b6aac5
tomer <tomeredel@gmail.com>
2025-03-12 11:33:03 +0200

    Implement the basic Flask app

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # Implemented by Tomer
+@app.route("/")
+def home():
+    return render_template("index.html")
+
 @app.route("/GET /<room>", methods= ['GET'])
 def room(room):
     return render_template("index.html")
